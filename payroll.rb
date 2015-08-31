fd279456dad29441c62be3840ece551bd7063950
Korwin Home <korwin.stevens@gmail.com>
2015-08-30 20:33:38 -0400

    added salaried and hourly subclasses

--- payroll.rb
+++ payroll.rb
@@ -1,6 +1,6 @@
 class Employee
 
-  attr_reader :name, :salary
+  attr_reader :name
 
 #set the name
   def name=(name)
@@ -10,7 +10,23 @@ class Employee
     @name = name
   end
 
-#set the salary
+  def print_name
+    puts "Name: #{@name}"
+  end
+=begin
+#init the class
+  def initialize(name = "Anon", salary = 0.0)
+    self.name = name
+    self.salary = salary
+
+  end
+=end
+end
+
+class SalariedEmployee < Employee
+  attr_reader :salary
+
+  #set the salary
   def salary=(salary)
     if salary < 0
       raise "A salary of #{salary} isn't valid!"
@@ -18,17 +34,39 @@ class Employee
     @salary = salary
   end
 
-#init the class
-  def initialize(name = "Anon", salary = 0.0)
-    self.name = name
-    self.salary = salary
-
+  #print the pay stub
+  def print_pay_stub
+    print_name
+    pay_for_period = (@salary / 365.0) * 14
+    formatted_pay = format("%.2f", pay_for_period)
+    puts "Pay for this period: $#{formatted_pay}"
   end
 
-#print the pay stub
+end
+
+class HourlyEmployee < Employee
+
+  attr_reader :hourly_wage, :hours_per_week
+
+  #wage calc
+  def hourly_wage=(hourly_wage)
+    if hourly_wage < 0
+      raise "A wage of #{hourly_wage} is invalid!"
+    end
+    @hourly_wage = hourly_wage
+  end
+
+  def hours_per_week=(hours_per_week)
+    if hours_per_week < 0
+      raise "#{hours_per_week} is invalid!"
+    end
+    @hours_per_week = hours_per_week
+  end
+
+  #print the pay stub
   def print_pay_stub
-    puts "Name: #{@name}"
-    pay_for_period = (@salary / 365.0) * 14
+    print_name
+    pay_for_period = @hourly_wage * @hours_per_week * 2
     formatted_pay = format("%.2f", pay_for_period)
     puts "Pay for this period: $#{formatted_pay}"
   end
@@ -39,12 +77,13 @@ end
 # YOU ARE NOW ENTERING THE TEST ZONE
 # Comment or uncomment as needed for code testing
 
-blake = Employee.new
+blake = SalariedEmployee.new
 blake.name = "Blake Black"
 blake.salary = 140000
 blake.print_pay_stub
 
-carl = Employee.new
-#carl.name = "Carl Cooper"
-#carl.salary = 40000
+carl = HourlyEmployee.new
+carl.name = "Carl Cooper"
+carl.hourly_wage = 17
+carl.hours_per_week = 38.5
 carl.print_pay_stub
